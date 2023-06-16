1242fdb95c48185bd3c5111be705660220c1b12d
SAMPREET-SHARMA <83722864+SAMPREET-SHARMA@users.noreply.github.com>
2023-06-16 22:09:14 +0530

    Code-anagram.js
    solution for anagram problem in js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -9,6 +9,21 @@
 
 function isAnagram(str1, str2) {
 
+if(str1.length!=str2.length){
+  console.log("The given words are not anagrams!");
+} else {
+  var count = 0;
+  for(i=0;i<str1.length;i++){
+    for(j=0;j<str2.length;j++){
+      if(str1[i]==str2[j]){
+        count=count+1;
+      }
+    }
+  }
+  count == str1.length ? console.log("The given words are anagrams") : console.log("The given words are not anagrams");
+}
 }
 
 module.exports = isAnagram;
+
+isAnagram("care","race");
